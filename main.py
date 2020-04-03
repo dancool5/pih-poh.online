96e18018703d8eedfb393807cf7a3765e9c75f8b
dancool_5 <dancool_5@mail.ru>
2020-04-03 17:48:02 +0500

    Update base model and css style too (to do them more or less adapted)

--- main.py
+++ main.py
@@ -8,6 +8,17 @@ app = Flask(__name__)
 def main_page():
     return render_template('base.html', title='pih-poh.online')
 
+@app.route('/forum')
+def forum():
+    return render_template('base.html', title='Форум')
+
+@app.route('/about')
+def about():
+    return render_template('base.html', title='О проекте')
+
+@app.route('/donat')
+def donat():
+    return render_template('base.html', title='Донат')
 
 if __name__ == '__main__':
     app.run(port=5000, host='127.0.0.1')
