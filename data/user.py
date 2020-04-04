b3f07b2cbc35f763835e18bc02caeb96ad3fa1a3
dancool_5 <dancool_5@mail.ru>
2020-04-04 14:19:05 +0500

    Add class user

---
+++ data/user.py
@@ -0,0 +1,16 @@
+import datetime
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    nickname = sqlalchemy.Column(sqlalchemy.String, unique=True)
+    about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    birth_date = sqlalchemy.Column(sqlalchemy.DateTime)
+    statuses = sqlalchemy.Column(sqlalchemy.String, nullable=True, default='')
+    email = sqlalchemy.Column(sqlalchemy.String, index=True, unique=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
