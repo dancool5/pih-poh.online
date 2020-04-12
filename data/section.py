0a9fc3b345db6f9d06e9502a98bbd12122c05dde
dancool_5 <dancool_5@mail.ru>
2020-04-12 20:53:39 +0500

    Start to do template of sections

--- data/section.py
+++ data/section.py
@@ -9,6 +9,7 @@ class Section(SqlAlchemyBase):
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     name = sqlalchemy.Column(sqlalchemy.String)
+    address = sqlalchemy.Column(sqlalchemy.String)
     description = sqlalchemy.Column(sqlalchemy.String)
     count_threads = sqlalchemy.Column(sqlalchemy.Integer, default=0)
     active_threads = sqlalchemy.Column(sqlalchemy.Integer, default=0)
