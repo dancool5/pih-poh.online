67acdef81f43cadef26cc776efa5a7d1abf42270
dancool_5 <dancool_5@mail.ru>
2020-05-08 16:29:29 +0500

    Bug fix

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import io
+from os import path
 
 from flask_mail import Mail
 
@@ -357,7 +358,7 @@ def user_avatar(user_id, cash_number):
     return send_file(io.BytesIO(open('static/images/avatar.png', 'rb').read()), mimetype='image/*')
 
 
-db_session.global_init("db/pihpoh_db.sqlite")
+db_session.global_init(path.join(path.dirname(__file__), './db/mars_explorer.db'))
 
 db = db_session.create_session()
 
