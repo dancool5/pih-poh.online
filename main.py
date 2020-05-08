9a160c7a9da6eb28165a7671eb4161ec75f55a1e
dancool_5 <dancool_5@mail.ru>
2020-05-08 13:10:26 +0500

    Bug fix

--- main.py
+++ main.py
@@ -365,4 +365,4 @@ if len(db.query(Section).all()) == 0:
     seed()
 
 if __name__ == '__main__':
-    app.run(port=5000, host='127.0.0.1')
+    app.run(port=os.environ.get('PORT'), host='127.0.0.1')
