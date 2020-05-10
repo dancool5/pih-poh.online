0878338a4eb26a65c520248e3c41d8c3af92c933
dancool_5 <dancool_5@mail.ru>
2020-05-10 18:50:46 +0500

    Fix bug

--- data/db_session.py
+++ data/db_session.py
@@ -17,11 +17,12 @@ def global_init(db_file):
     if not db_file or not db_file.strip():
         raise Exception('Необходимо указать файл базы данных.')
 
-    conn_str = f'sqlite:///{db_file.strip()}?check_same_thread=False'
+    conn_str_sqlite = f'sqlite:///{db_file.strip()}?check_same_thread=False'
+    conn_str = os.environ.get('DATABASE_URL', conn_str_sqlite)
     print(f'Подключение к базе данных по адресу {conn_str}')
 
-    engine = sqlalchemy.create_engine(conn_str, echo=False)
-    __factory = orm.sessionmaker(bind=engine, autoflush=False)
+    engine = sqlalchemy.create_engine(conn_str, echo=False, pool_size=10,  max_overflow=20)
+    __factory = orm.sessionmaker(bind=engine)
 
     from . import __all_models
     SqlAlchemyBase.metadata.create_all(engine)
@@ -30,4 +31,3 @@ def global_init(db_file):
 def create_session() -> orm.Session:
     global __factory
     return __factory()
-
